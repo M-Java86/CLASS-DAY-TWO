3b010500ad11e7911efd8943fd0877885a057435
M-Java86 <mpythondata@outlook.com>
2023-02-15 18:36:35 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,63 +1,62 @@
 import React from 'react';
 import './App.css';
-import TodoContainer from './components/TodoContainer'
-import TodoInput from './components/TodoInput'
+import TodoContainer from './components/TodoContainer';
+import TodoInput from './components/TodoInput';
 
-class App extends React.Component{
-
+class App extends React.Component {
   //todo app structure {status, description, title, id}
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      todos:[
+    this.state = {
+      todos: [
         {
           id: 1,
-          title: "test title",
-          description: "another test descripion",
-          status: 'PENDING'
-      }
-    ],
-      currentlyEditingTodo: null
-    }
-    this.addTodo = this.addTodo.bind(this)
+          title: 'test title',
+          description: 'another test descripion',
+          status: 'PENDING',
+        },
+      ],
+      currentlyEditingTodo: null,
+    };
+    this.addTodo = this.addTodo.bind(this);
   }
 
-  addTodo(todoObject){
+  addTodo(todoObject) {
     debugger;
-    const newTodos = JSON.parse(JSON.stringify(this.state.todos))
-    newTodos.push(todoObject)
+    const newTodos = JSON.parse(JSON.stringify(this.state.todos));
+    newTodos.push(todoObject);
     return this.setState({
-      todos: newTodos
-    })
+      todos: newTodos,
+    });
   }
 
-  editTodo(){
+  editTodo() {
     // finish me
   }
 
-  deleteTodo(){
+  deleteTodo() {
     // finish me
   }
 
-  handleEditCallBack = todo => this.setState({currentlyEditingTodo: todo})
+  handleEditCallBack = (todo) => this.setState({ currentlyEditingTodo: todo });
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <TodoInput
           todo={this.state.currentlyEditingTodo}
           todoAmount={this.state.todos.length}
           handleSaveClick={this.addTodo}
-          buttonText={this.state.currentlyEditingTodo? 'Edit':'Add'}
+          buttonText={this.state.currentlyEditingTodo ? 'Edit' : 'Add'}
         />
         <TodoContainer
-          todos = {this.state.todos}
-          handleEdit = {this.editTodo}
-          handleDelete = {this.deleteTodo}
+          todos={this.state.todos}
+          handleEdit={this.editTodo}
+          handleDelete={this.deleteTodo}
         />
       </div>
     );
   }
-}
+} //find
 
 export default App;
